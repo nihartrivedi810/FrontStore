d0819fec5fe968173928257629cd9964d54293eb
achut <achut93@gmail.com>
2016-02-19 11:45:29 +0530

    toolbar name

--- scripts/videoPage.js
+++ scripts/videoPage.js
@@ -9,13 +9,13 @@ function changeView(){
 }
 
 var resizeWindow = function () {
-	var first = document.getElementById("toolbar-top"),
-		second = document.getElementById("toolbar-editor"),
+	var toolbarTop = document.getElementById("toolbar-top"),
+		toolbarBottom = document.getElementById("toolbar-editor"),
 		container = document.getElementById("advance-wrapper"),
-		firstHeight = first.offsetHeight,
-		containerHeight = container.offsetHeight;
+		toolbarTopHeight = toolbarTop.offsetHeight,
+		containerHeight = toolbarBottom.offsetHeight;
 
-	second.setAttribute("style","height:"+(containerHeight - firstHeight -5) +"px");
+	toolbarBottom.setAttribute("style","height:"+(containerHeight - toolbarTopHeight -5) +"px");
 }
 
 resizeWindow();
