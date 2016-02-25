3585e4eeefbc08b6fc7593009a4e51af98f3649f
Nihar Trivedi <nihartrivedi810@Nihars-MacBook-Pro.local>
2016-02-25 15:48:26 +0530

    changes in videoText,js after review

--- scripts/localStorageUtil.js
+++ scripts/localStorageUtil.js
@@ -13,3 +13,19 @@ function localStorageSet(name,value)
 {
 	localStorage[name]=JSON.stringify(value);
 };
+
+function getAllParameters(url){
+	if(url===undefined){
+		return;
+	}
+	var urlArraySplit1 = url.split("?");
+	var urlArraySplit2 =urlArraySplit1[1].split("&");
+	//console.log(urlArraySplit1,urlArraySplit2)
+	var parameters = {};
+	var paramVal;
+	urlArraySplit2.forEach(function(parameter){
+		paramVal = parameter.split("=");
+		parameters[paramVal[0]] = decodeURIComponent(paramVal[1]);
+	});
+	return parameters;
+}
--- scripts/videoText.js
+++ scripts/videoText.js
@@ -9,34 +9,24 @@ $(function(){
 	var videoList,currentVideo;
 	var model = {
 		isSaved :false,
-		init: function() {
-			var params = model.getAllParameters();
-			model.topicId=parseInt(params["topic"])
-			model.topic = Courses[model.topicId];
-			model.lesson = Lessons[parseInt(params["lesson"])];
-			videoList=model.lesson.videos;
-			currentVideo = Videos[videoList[0]];
-			if(!model.topic||!model.lesson||!videoList)
-			{
-				
+		init: function(topicId,lessonId) {
+			
+			if(isNaN(topicId)||isNaN(lessonId)||!(model.topic = Courses[topicId])||!(model.lesson = Lessons[lessonId])||!(videoList = model.lesson.videos)||Lessons[lessonId]["courseId"]!==+topicId){
 				$(location).attr('href', 'homepage.html');
 			}
-			
-			//model.topicName = model.topic.name;
+			model.topicId=topicId;
+			currentVideo = Videos[videoList[0]];
+
 			if (!localStorageGet('notes')) {
 				localStorageSet('notes',{});
 			}
-
-			// console.log("came");
 		},
 		addNewNote: function(obj) {
 			var data =localStorageGet('notes');
-			if(data[currentVideo.id])
-			{
+			if(data[currentVideo.id]){
 				data[currentVideo.id].notes=obj;
 			}
-			else
-			{
+			else{
 				data[currentVideo.id]={ notes: obj};	
 			}
 			localStorageSet('notes',data);
@@ -44,24 +34,20 @@ $(function(){
 		},
 		addNewjsbin: function(jsbinURL) {
 			var data =localStorageGet('notes');
-			if(data[currentVideo.id])
-			{
+			if(data[currentVideo.id]){
 				data[currentVideo.id].jsbin=jsbinURL;
 			}
-			else
-			{
+			else{
 				data[currentVideo.id]={ jsbin: jsbinURL};	
 			}
 			localStorageSet('notes',data);
 		},
 		getNotesOfCurrentVideo: function() {
 			var data=localStorageGet('notes');
-			if(data[currentVideo.id])
-			{
+			if(data[currentVideo.id]){
 				return data[currentVideo.id].notes;	
 			}
-			else
-			{
+			else{
 				return false;
 			}
 		},
@@ -84,8 +70,8 @@ $(function(){
 			return currentVideo.id;
 		},
 		getAllVideos: function() {
-			var videos = videoList.map(function (vId){
-				return Videos[vId];
+			var videos = videoList.map(function (videoId){
+				return Videos[videoId];
 			})
 			return videos;
 		},
@@ -97,33 +83,14 @@ $(function(){
 		},
 		getCurrentTopicName: function(){
 			return model.topic.name;
-		},
-		// getParameterByName: function(name, url) {
-		// 	if (!url) url = window.location.href;
-		// 	name = name.replace(/[\[\]]/g, "\\$&");
-		// 	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		// 	results = regex.exec(url);
-		// 	if (!results) return null;
-		// 	if (!results[2]) return '';
-		// 	return decodeURIComponent(results[2].replace(/\+/g, " "));
-		// }
-		getAllParameters: function(){
-			var urlArraySplt1 = (window.location.href).split("?");
-			var urlArraySplt2 =urlArraySplt1[1].split("&");
-			var parameters = {};
-			var paramVal;
-			urlArraySplt2.forEach(function(parameter){
-				paramVal = parameter.split("=");
-				parameters[paramVal[0]] = decodeURIComponent(paramVal[1]);
-			});
-			return parameters;
 		}
 	};
 
 
 	var octopus = {
 		init: function() {
-			model.init();
+			var params = getAllParameters(window.location.href);
+			model.init(params["topic"],params["lesson"]);
 
 			view.init();
 
@@ -175,7 +142,7 @@ $(function(){
 		isSaved: function(){
 			return model.isSaved;
 		},
-		save: function(val){
+		toggleSave: function(val){
 			model.isSaved = val;
 		},
 		enableJSButton: function() {
@@ -196,8 +163,8 @@ $(function(){
 		renderThreeViews: function() {
 			view.renderThreeViews();
 		},
-		changeView: function(){
-			view.changeView();
+		toggleView: function(){
+			view.toggleView();
 		}
 	};
 	var sidePanelView = {
@@ -219,25 +186,25 @@ $(function(){
 
 			parent.append(listAppend);
 
-			optSign.on("click", function(e) {
+			optSign.on("click", function(event) {
 				sideBlk.css('transform','translateX(0%)');
 				optSign.css("visibility", "hidden");
 				$('#content-styler').css('opacity','0.1');
 				mainDiv.css("display" ,'block');
 			});
 
-			$("#l1").on("click", function(e) {
-				if(e.target && e.target.nodeName == "LI") {
+			$("#l1").on("click", function(event) {
+				if(event.target && event.target.nodeName === "LI") {
 					sideBlk.css('transform','translateX(-100%)');
 					optSign.css("visibility", "visible");
 					$('#content-styler').css('opacity','1');
 					mainDiv.css("display" ,'none');
-					octopus.changeCurrentVideo(e.target.id);
+					octopus.changeCurrentVideo(event.target.id);
 				}
 			});
 
-			mainDiv.on("click", function(e) {
-				if(e.target.id != 'bar'){
+			mainDiv.on("click", function(event) {
+				if(event.target.id != 'bar'){
 					sideBlk.css('transform','translateX(-100%)');
 					optSign.css("visibility", "visible");
 					$('#content-styler').css('opacity','1');
@@ -272,21 +239,18 @@ $(function(){
 	};
 	var notesView = {
 		init: function() {
-			$('#toolbar-top').on('click',function(e){
-				if(e.target.id=="save-notes")
-				{
+			$('#toolbar-top').on('click',function(event){
+				if(event.target.id==="save-notes"){
 					a=advancedEditor.getContents();
 					octopus.addNewNote(a);
 					//$("#save-notes").attr('class','save-notes-btn--grey');
 					$("#save-notes").attr('disabled','true');
 				}
-				else if(e.target.id=="embed-bin-btn")
-				{
+				else if(event.target.id==="embed-bin-btn"){
 					octopus.showModal();
 				}
-				else if(e.target.id=="change-view")
-				{
-					octopus.changeView();
+				else if(event.target.id==="change-view"){
+					octopus.toggleView();
 				}
 			});
 			advancedEditor.on("text-change",function(delta){
@@ -294,21 +258,19 @@ $(function(){
 					$("#save-notes").removeAttr('disabled');
 					//$("#save-notes").attr('class','save-notes-btn');
 				}
-				octopus.save(false);
+				octopus.toggleSave(false);
 			});
 			this.render();
 		},
 		render: function() {
 			var notes=octopus.getNotesOfCurrentVideo();
-			if(notes)
-			{
-				octopus.save(true);
+			if(notes){
+				octopus.toggleSave(true);
 				advancedEditor.setContents(notes);
 			}
-			else
-			{
+			else{
 				notes=[];
-				octopus.save(true);
+				octopus.toggleSave(true);
 				advancedEditor.setContents(notes);
 			}
 		},
@@ -323,13 +285,11 @@ $(function(){
 	};
 	var modalView = {
 		init: function() {
-			$('#modal').on('click',function(e){
-				if(e.target.id=='hide-jsbin')
-				{
+			$('#modal').on('click',function(event){
+				if(event.target.id==='hide-jsbin'){
 					modalView.hidemodal();
 				}
-				else if(e.target.id=='saveUrl')
-				{
+				else if(event.target.id==='saveUrl'){
 					var t2=$("#jsbinUrl").val();
 					octopus.addNewjsbin(t2);
 					$('#embed-bin-btn').eq(0).attr("disabled",true);
@@ -353,15 +313,13 @@ $(function(){
 		},
 		render: function() {
 			var jsbin=octopus.getjsbinOfCurrentVideo();
-			if(jsbin)
-			{
+			if(jsbin){
 				octopus.disableJSButton();
 				$("iframe").remove();
 				this.jsbintag.append('<iframe src=' + jsbin + ' style="border: 1px solid rgb(170, 170, 170); width: 100%; height:100%;min-height: 300px;"></iframe>');
 				octopus.renderThreeViews();
 			}
-			else
-			{
+			else{
 				octopus.renderTwoViews();
 				octopus.enableJSButton();
 				$("iframe").remove();
@@ -391,7 +349,7 @@ $(function(){
 			view.resizeWindow();
 			octopus.hidemodal();
 		},
-		changeView: function(){
+		toggleView: function(){
 			var div = document.getElementById("content-styler");
 			if(div.className==="content-style-1")
 				div.className="content-style-2";
