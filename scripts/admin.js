fd79c4f820c421c94ea41439c2be0efdb8e4e3dd
vedanttikku12 <vedant.tikku@gmail.com>
2016-03-01 17:19:27 +0530

    arrows added

--- scripts/admin.js
+++ scripts/admin.js
@@ -192,7 +192,9 @@ var model = {
 
 			this.lessonWrapEl = document.createElement("div");
 			this.lessonWrapEl.setAttribute("class","lesson-wrapper");
-
+            var arrow = document.createElement("div");
+            arrow.setAttribute("class","arrow");
+            this.lessonWrapEl.appendChild(arrow);
 			this.lessonTitleEl = document.createElement("div");
 			this.lessonTitleEl.setAttribute("class","lesson-name");
 
@@ -204,24 +206,28 @@ var model = {
 			this.videoContainerEl = document.createElement("div");
 			this.videoContainerEl.setAttribute("class","video-wrapper");
 			this.videoContainerEl.innerHTML = "<button class=\"video-add\" data-course="+lesson.getId()+">Add Video</button> <div class=\"video__add__input-contain\" ><input type=\"text\" id=\"input_new_video\"><button class=\"video__add_input-contain__button\"data-lesson="+lesson.getId()+">Add Video</button> ";
-			this.videoWrapperEl.setAttribute("class","video-container");
+			//this.videoWrapperEl.setAttribute("class","video-container");
 
 			controller.createVideo(lesson);
-			console.log(this.videoContainerEl,this.videoWrapperEl);
+			//console.log(this.videoContainerEl,this.videoWrapperEl);
 
 			this.lessonWrapEl.appendChild(this.videoContainerEl);
 			this.lessonContainerEl.appendChild(this.lessonWrapEl);
 		},
 
 		renderVideo : function (video) {
-			this.videoBox = document.createElement("div");
+			var videoBox = document.createElement("div");
+            videoBox.setAttribute("class", "video-container");
+            var arrow = document.createElement("div");
+            arrow.setAttribute("class","arrow");
+            videoBox.appendChild(arrow);
 			this.videoWrapper = document.createElement("div");
 			this.videoWrapper.setAttribute("class","lesson-wrapper__videos");
 
 			this.addAttributes(viewDisplay.videoWrapper,video.getUrl(),"video","false",video.getId());
 			this.videoWrapper.innerHTML = video.getName();
-
-			this.videoContainerEl.appendChild(this.videoWrapper);
+            videoBox.appendChild(this.videoWrapper);
+			this.videoContainerEl.appendChild(videoBox);
 		},
 
 		// if i die then it is because of refactoring this function.
@@ -255,7 +261,7 @@ var model = {
 					case "inner-content__course-card__course":
 					var sibling = event.target.nextSibling;
 					if(sibling) {
-						sibling.style.height = (sibling.clientHeight == 0 ? "221px":"0");
+						sibling.style.maxHeight = (sibling.clientHeight == 0 ? "221px":"0");
 					}
 					break;
 
